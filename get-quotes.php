cbf65151b8d52cfe94d2a898e836ae32dcbe729f
austin495 <asimnoman2000@gmail.com>
2026-07-28 21:44:10 +0500

    commit

--- get-quotes.php
+++ get-quotes.php
@@ -129,6 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             flex-direction: row;
             align-items: center;
             justify-content: center;
+            padding: 100px 0;
             height: auto;
         }
 
